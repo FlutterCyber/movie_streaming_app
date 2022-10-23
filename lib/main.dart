1742d708c7356e0de30df34d57d81666d8d57b4a
fluttercyber <ronaldrask1@gmail.com>
2022-10-23 19:18:19 +0500

    new sdk version

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:movie_streaming_app/pages/home_page.dart';
+import 'package:movie_streaming_app/pages/lottie_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: LottiePage(),
     );
   }
 }
