9dafc407826428adf02944df7c53bc89f5e34a38
SardorIbrokhimov <ibrokhimovsardor872@gmail.com>
2022-10-24 10:07:08 +0500

    Appbar

---
+++ lib/resources/home_screen_resources.dart
@@ -0,0 +1,10 @@
+class HomeScreenResources {
+  static List<String> category = [
+    "Fantastik",
+    "Komediya",
+    "Drama",
+    "Bestseller",
+    "Multfilm"
+  ];
+  static String appbarMovie = "No Time To Die";
+}
