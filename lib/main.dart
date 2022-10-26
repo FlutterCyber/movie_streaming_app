bdbd567eeecbac0af3c9ae3be1abcd8f2806c25d
SardorIbrokhimov <ibrokhimovsardor872@gmail.com>
2022-10-27 00:14:13 +0500

    the last update time 14.00 28.10.2022

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:movie_streaming_app/for%20test/timer_image.dart';
 import 'package:movie_streaming_app/pages/hammasi%20shu%20yerda%20page.dart';
 import 'package:movie_streaming_app/pages/home_page.dart';
 import 'package:movie_streaming_app/pages/login_pages/sign%20up.dart';
 import 'package:movie_streaming_app/pages/login_pages/sing%20in.dart';
-import 'package:movie_streaming_app/pages/login_pages/start_page.dart';
 import 'package:movie_streaming_app/pages/navbar_pages/home.dart';
 import 'package:movie_streaming_app/pages/navbar_pages/playlist.dart';
 import 'package:movie_streaming_app/scroll_test.dart';
@@ -20,17 +20,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+       primarySwatch: Colors.red,
       ),
-      home: const StartPage(),
+      home: const AllPage(),
       routes: {
-        StartPage.id: (context) => const StartPage(),
         HomePage.id: (context) => const HomePage(),
         HomeScreen.id: (context) => const HomeScreen(),
         PlaylistPage.id: (context) => const PlaylistPage(),
         SignInPage.id: (context) => const SignInPage(),
-        SignUpPage.id: (context) => const SignUpPage(),
+        SignUpPage.id: (context) => const SignUpPage()
       },
     );
   }
 }
+
